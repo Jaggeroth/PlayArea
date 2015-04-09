18119d7903e0eb6b9121c23f649e59c265456eb4
David Rawlins <david@hero-id.co.uk>
2015-04-09 13:26:28 +0100

    Added 9 and 10

--- src/playarea/javafx/AppTestTwoController.java
+++ src/playarea/javafx/AppTestTwoController.java
@@ -44,6 +44,8 @@ public class AppTestTwoController {
         hm.put("Location 6", new ArrayList<>(Arrays.asList(658.0, 118.0, "Location 6")));
         hm.put("Location 7", new ArrayList<>(Arrays.asList(658.0, 118.0, "Location 7")));
         hm.put("Location 8", new ArrayList<>(Arrays.asList(658.0, 118.0, "Location 8")));
+        hm.put("Location 9", new ArrayList<>(Arrays.asList(658.0, 118.0, "Location 9")));
+        hm.put("Location 10", new ArrayList<>(Arrays.asList(658.0, 118.0, "Location 10")));
 
         ObservableList<String> names = FXCollections.observableArrayList();
         Set<Entry<String, ArrayList<Comparable<?>>>> set = hm.entrySet();
