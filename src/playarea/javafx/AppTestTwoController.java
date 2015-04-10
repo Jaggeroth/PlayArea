ecd7210ea58e45f964d88adc96e49c554983b8f3
Jaggeroth <david@hero-id.co.uk>
2015-04-10 21:55:01 +0100

    Add description area

--- src/playarea/javafx/AppTestTwoController.java
+++ src/playarea/javafx/AppTestTwoController.java
@@ -16,6 +16,7 @@ import javafx.animation.Timeline;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.ScrollPane;
@@ -30,22 +31,24 @@ public class AppTestTwoController {
     private ListView<String> map_listview;
     @FXML
     private ScrollPane map_scrollpane;
+    @FXML
+    private Label location_detail; 
 
     private final HashMap<String, ArrayList<Comparable<?>>> hm = new HashMap<>();
 
     @FXML
     void initialize() {
 
-        hm.put("Location 1", new ArrayList<>(Arrays.asList(424.0, 120.0, "Location 1")));
-        hm.put("Location 2", new ArrayList<>(Arrays.asList(466.0, 178.0, "Location 2")));
-        hm.put("Location 3", new ArrayList<>(Arrays.asList(400.0, 276.0, "Location 3")));
-        hm.put("Location 4", new ArrayList<>(Arrays.asList(326.0, 280.0, "Location 4")));
-        hm.put("Location 5", new ArrayList<>(Arrays.asList(658.0, 118.0, "Location 5")));
-        hm.put("Location 6", new ArrayList<>(Arrays.asList(595.0, 158.0, "Location 6")));
-        hm.put("Location 7", new ArrayList<>(Arrays.asList(615.0, 204.0, "Location 7")));
-        hm.put("Location 8", new ArrayList<>(Arrays.asList(810.0, 193.0, "Location 8")));
-        hm.put("Location 9", new ArrayList<>(Arrays.asList(747.0, 405.0, "Location 9")));
-        hm.put("Location 10", new ArrayList<>(Arrays.asList(587.0, 298.0, "Location 10")));
+        hm.put("Tower of Waiting", new ArrayList<>(Arrays.asList(424.0, 120.0, "Location 1")));
+        hm.put("Upper Quays", new ArrayList<>(Arrays.asList(466.0, 178.0, "Location 2")));
+        hm.put("Five-Arched Bridge", new ArrayList<>(Arrays.asList(400.0, 276.0, "Location 3")));
+        hm.put("Nentir Inn", new ArrayList<>(Arrays.asList(326.0, 280.0, "Location 4")));
+        hm.put("Knight's Gate", new ArrayList<>(Arrays.asList(658.0, 118.0, "Location 5")));
+        hm.put("Silver Unicorn Inn", new ArrayList<>(Arrays.asList(595.0, 158.0, "Location 6")));
+        hm.put("Half Moon Trading House", new ArrayList<>(Arrays.asList(615.0, 204.0, "Location 7")));
+        hm.put("Moonstone Keep", new ArrayList<>(Arrays.asList(810.0, 193.0, "Location 8")));
+        hm.put("Tombwood", new ArrayList<>(Arrays.asList(747.0, 405.0, "Location 9")));
+        hm.put("House of the Sun", new ArrayList<>(Arrays.asList(587.0, 298.0, "Location 10")));
 
         ObservableList<String> names = FXCollections.observableArrayList();
         Set<Entry<String, ArrayList<Comparable<?>>>> set = hm.entrySet();
@@ -67,6 +70,7 @@ public class AppTestTwoController {
         double mapHeight = 1248;
         double scrollH = (Double) list.get(0) / mapWidth;
         double scrollV = (Double) list.get(1) / mapHeight;
+        location_detail.setText((String) list.get(2));
         final Timeline timeline = new Timeline();
         final KeyValue kv1 = new KeyValue(map_scrollpane.hvalueProperty(), scrollH);
         final KeyValue kv2 = new KeyValue(map_scrollpane.vvalueProperty(), scrollV);
