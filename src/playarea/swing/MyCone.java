76200fccf3433872b4f8a70f63704e2b9329c197
David Rawlins <david@hero-id.co.uk>
2015-04-24 13:40:35 +0100

    parameterise

--- src/playarea/swing/MyCone.java
+++ src/playarea/swing/MyCone.java
@@ -11,9 +11,13 @@ import javax.swing.JComponent;
 public class MyCone extends JComponent {
 
 	public void paint(Graphics g) {
+		double centreX = 150;
+		double centreY = 75;
+		double coneRadius = 150;
+		double discRadius = 30;
 	    Graphics2D g2 = (Graphics2D) g;
-		Arc2D.Double cone = new Arc2D.Double(0, 0, 300, 150, 0, -60, Arc2D.PIE);
-		Ellipse2D.Double disc = new Ellipse2D.Double(120, 60, 60, 30);
+		Arc2D.Double cone = new Arc2D.Double(centreX - coneRadius, centreY - (coneRadius/2), coneRadius*2, coneRadius, 0, -60, Arc2D.PIE);
+		Ellipse2D.Double disc = new Ellipse2D.Double(centreX - discRadius, centreY - (discRadius/2), discRadius*2, discRadius);
 		Area shape = new Area(cone);
 		shape.add(new Area(disc));
 		g2.draw(shape);
