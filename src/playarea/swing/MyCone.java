b721f455e518f77fc13b24d08a875018c8182710
David Rawlins <david@hero-id.co.uk>
2015-04-24 16:40:49 +0100

    Parameterise arc

--- src/playarea/swing/MyCone.java
+++ src/playarea/swing/MyCone.java
@@ -15,8 +15,10 @@ public class MyCone extends JComponent {
 		double centreY = 75;
 		double coneRadius = 150;
 		double discRadius = 30;
+		double facing = 135;
+		double arc = 60;
 	    Graphics2D g2 = (Graphics2D) g;
-		Arc2D.Double cone = new Arc2D.Double(centreX - coneRadius, centreY - (coneRadius/2), coneRadius*2, coneRadius, 0, -60, Arc2D.PIE);
+		Arc2D.Double cone = new Arc2D.Double(centreX - coneRadius, centreY - (coneRadius/2), coneRadius*2, coneRadius, facing-(arc/2), arc, Arc2D.PIE);
 		Ellipse2D.Double disc = new Ellipse2D.Double(centreX - discRadius, centreY - (discRadius/2), discRadius*2, discRadius);
 		Area shape = new Area(cone);
 		shape.add(new Area(disc));
